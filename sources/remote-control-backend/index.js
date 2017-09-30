b801f34e01fe6493319ce1189c761deb97b40e97
Pavel <pavel.nikitiuk@gmail.com>
2017-09-30 13:03:12 +0300

    rerange dependencies

--- sources/remote-control-backend/index.js
+++ sources/remote-control-backend/index.js
@@ -1,7 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
 
 const api = require('remote-control-api');
 const { get } = require('remote-control-config');
@@ -10,11 +7,7 @@ const { start: nrfStart } = require('remote-control-nrf');
 
 const app = express();
 
-app
-  .use(morgan('dev'))
-  .use(bodyParser.json())
-  .use(methodOverride())
-  .use('/', api);
+app.use('/', api);
 
 // connect to database
 connect(get('connectionString'));
--- sources/remote-control-backend/remote-control-api/index.js
+++ sources/remote-control-backend/remote-control-api/index.js
@@ -1,4 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+
 const api = require('express').Router();
+
+api
+.use(morgan('dev'))
+.use(bodyParser.json())
+.use(methodOverride());
+
 const recordings = require('./recordings');
 
 api.use('/recordings', recordings);
