affa5be358c64bd5f64855206e8c99a01e6b13a8
Pavel <pavel.nikitiuk@gmail.com>
2017-09-30 13:24:04 +0300

    base cors setup

--- sources/remote-control-app/index.js
+++ sources/remote-control-app/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const backend = require('remote-control-backend');
 const http = require('http');
+const cors = require('cors');
+
 const { get } = require('remote-control-config');
 const { logger } = require('remote-control-services');
+
+if (process.NODE_ENV !== 'production') {
+  backend.use(cors());
+}
 
 const app = express().use('/', backend);
 
