e91a5da0d937fc43a8ef10b2fafc9f9c6021db6d
Pavel <pavel.nikitiuk@gmail.com>
2017-10-02 00:50:50 +0300

    remove test code

--- sources/remote-control-frontend/source/actions/counter.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from 'redux-actions';
-
-import { INCREMENT, DECREMENT } from 'consts/counter';
-
-export const increment = createAction(INCREMENT);
-export const decrement = createAction(DECREMENT);
--- sources/remote-control-frontend/source/components/presentational/Section/index.js
+++ sources/remote-control-frontend/source/components/presentational/Section/index.js
@@ -1,8 +1,4 @@
-import http from 'services/http';
-
 import Section from './Section';
 import './section.css';
-
-http.recordings.temperature.getAll();
 
 export default Section;
