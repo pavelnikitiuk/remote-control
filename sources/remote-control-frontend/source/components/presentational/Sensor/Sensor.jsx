ad467f1d38e614517b0c170c0961360904bc3280
Pavel <pavel.nikitiuk@gmail.com>
2017-10-04 01:24:08 +0300

    add sensor type

--- sources/remote-control-frontend/source/components/presentational/Sensor/Sensor.jsx
+++ sources/remote-control-frontend/source/components/presentational/Sensor/Sensor.jsx
@@ -8,12 +8,20 @@ const circleProps = {
   T: { maxValue: 30, minValue: -10, maxColor: 'f49441', minColor: '41b8f4' },
 };
 
+const icon = {
+  T: <TermometrIcon className="sensor__icon" />,
+};
+
+const processWtihValue = {
+  T: value => Math.round(value * 100) / 100,
+};
+
 const Sensor = ({ name, value, type }) => (
   <Circle {...circleProps[type]} value={value}>
     <div className="sensor">
       <div className="sensor__value-container">
-        <TermometrIcon className="sensor__icon" />
-        <h1 className="sensor__value">{Math.round(value * 100) / 100}</h1>
+        {icon[type]}
+        <h1 className="sensor__value">{processWtihValue[type](value)}</h1>
       </div>
       <h3 className="sensor__name">{name}</h3>
     </div>
