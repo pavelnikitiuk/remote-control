91e0c0685a421837a3b72f4b6c5f5306ca665937
Pavel <pavel.nikitiuk@gmail.com>
2017-10-04 01:18:14 +0300

    add sensor type

--- sources/remote-control-frontend/source/components/presentational/Sensor/Sensor.jsx
+++ sources/remote-control-frontend/source/components/presentational/Sensor/Sensor.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import TermometrIcon from 'icons/thermometer.svg';
 import Circle from './../Сircles/ColoredCircle';
 
-import TermometrIcon from 'icons/thermometer.svg';
+const circleProps = {
+  T: { maxValue: 30, minValue: -10, maxColor: 'f49441', minColor: '41b8f4' },
+};
 
-const Sensor = ({ name, value }) => (
-  <Circle maxValue={30} minValue={-10} maxColor="f49441" minColor="41b8f4" value={value}>
+const Sensor = ({ name, value, type }) => (
+  <Circle {...circleProps[type]} value={value}>
     <div className="sensor">
       <div className="sensor__value-container">
-        <TermometrIcon width={40} height={40} />
+        <TermometrIcon className="sensor__icon" />
         <h1 className="sensor__value">{Math.round(value * 100) / 100}</h1>
       </div>
       <h3 className="sensor__name">{name}</h3>
@@ -20,10 +23,12 @@ const Sensor = ({ name, value }) => (
 Sensor.propTypes = {
   name: PropTypes.string.isRequired,
   value: PropTypes.number,
+  type: PropTypes.oneOf(['T']),
 };
 
 Sensor.defaultProps = {
   value: 0,
+  type: 'T',
 };
 
 export default Sensor;
