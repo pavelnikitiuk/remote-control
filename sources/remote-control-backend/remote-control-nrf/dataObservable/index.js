f1a07dd27f4eaecdad336f58f0f300527dbc2603
Pavel Nikitsiuk <Pavel_Nikitsiuk@epam.com>
2019-03-12 12:54:55 +0300

    inject dependencies in data observables

--- sources/remote-control-backend/remote-control-nrf/dataObservable/index.js
+++ sources/remote-control-backend/remote-control-nrf/dataObservable/index.js
@@ -1,11 +1,24 @@
-const { fromEventPattern } = require('rxjs');
+const { fromEventPattern, Subject, queueScheduler, from } = require('rxjs');
+const { map, mergeMap, observeOn, subscribeOn } = require('rxjs/operators');
 const dataHandlers = require('./handlers');
 
-module.exports = function createObservable(nrf) {
+const observable$ = new Subject();
+
+module.exports = function createObservable(nrf, dependencies = {}) {
   const nrf$ = fromEventPattern(
     (handler) => nrf.on('onDataReceived', handler),
     (handler) => nrf.off('onDataReceived', handler)
   );
-
-  nrf$.pipe(dataHandlers).subscribe();
+  observable$
+    .pipe(
+      map((x) => x(nrf$, dependencies)),
+      mergeMap((output$) =>
+        from(output$).pipe(
+          subscribeOn(queueScheduler),
+          observeOn(queueScheduler)
+        )
+      )
+    )
+    .subscribe(/* TODO: nrf.sendMessage() */);
+  observable$.next(dataHandlers);
 };
