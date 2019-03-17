d75568c81164894656adcb59ba7ccbeaa3ef02a4
Pavel Nikitsiuk <pavel.nikitiuk@gmail.com>
2019-03-17 21:21:07 +0300

    remove old models

--- sources/remote-control-backend/remote-control-database/models/Device.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const Device = new Schema({
-  Name: String,
-  sensors: [{ type: Schema.Types.ObjectId, ref: 'Sensor' }],
-});
-
-module.exports = mongoose.model('Device', Device);
